377ab3fa401975de08e39673e9dbcc3a5cb44a0d
dvbarros <davisilva.sousa14@outlook.com>
2024-05-19 17:40:38 -0300

    Implementando Pesquisa

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -179,6 +179,43 @@ conexao.connect(function(erro){
             });
         });
 
+        //Rota pra Buscar Produtos
+        app.get('/resultadoPesquisa', function(req, res) {
+            let productName = req.query.name;
+        
+            if (!productName) {
+                return res.status(400).send('Nome do produto não fornecido');
+            }
+        
+            console.log(`Buscando por: ${productName}`);
+        
+            let sql = `SELECT * FROM produtos WHERE name LIKE ?`;
+            let query = '%' + productName + '%';
+        
+            console.log(`Executando SQL: ${sql} com query: ${query}`);
+        
+            conexao.query(sql, [query], function(erro, resultadosBusca) {
+                if (erro) {
+                    console.error('Erro ao executar a consulta:', erro);
+                    res.status(500).send('Erro ao buscar!');
+                } else {
+                    
+                    const groupedResultadosBusca = [];
+                    for (let i = 0; i < resultadosBusca.length; i += 3) {
+                        groupedResultadosBusca.push(resultadosBusca.slice(i, i + 3));
+                    }
+                    console.log(groupedResultadosBusca)
+                    res.render('resultadoPesquisa', {
+                        title: 'Pesadão',
+                        groupedResultadosBusca: groupedResultadosBusca
+                    });
+                }
+            });
+        });
+        
+        
+        
+
 //ROTAS POST
         //Rota Cadastrar
         app.post('/cadastrar', function(req,res){
